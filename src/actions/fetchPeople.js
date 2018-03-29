aba7f23a519787a52ba9398e2c539970e7353088
mihir20 <mihir20121997@gmail.com>
2018-03-30 04:18:56 +0530

    fetchPeople action created

---
+++ src/actions/fetchPeople.js
@@ -0,0 +1,33 @@
+import * as types from './types'
+
+export const fetchPeopleFromApi = () =>{
+   return (dispatch) => {
+       dispatch(getPeople())
+       fetch('https://swapi.co/api/people/').then(
+           res => res.json()
+       ).then(
+         json => dispatch(getPeopleSuccess(json.result))
+       ).catch(
+           err => dispatch(getPeopleFail(err))
+       )
+   }
+}
+
+export const getPeople = () => {
+    return {
+        type:types.FETCHING_PEOPLE,
+    }
+}
+
+export const getPeopleSuccess = (data) => {
+    return{
+        type:types.FETCHING_PEOPLE_SUCCESS,
+        data,
+    }
+}
+
+export const getPeopleFail = () => {
+    return{
+        type:types.FETCHING_PEOPLE_FAIL,
+    }
+}
