0249f79033ba0875ed8ac9a3ea43e132d85046f4
mihir20 <mihir20121997@gmail.com>
2018-03-30 06:09:36 +0530

    loading people to text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import {
+  StyleSheet,
   Button,
-  View
+  View,
+  Text
 } from 'react-native';
 import {connect} from 'react-redux';
 import { fetchPeopleFromApi } from './actions/fetchPeople';
 
 
 const App = (props) =>{
-  console.log("people: ",props.people)
+  const { people, isFetching } = props.people;
+  console.log("people: ",props.people);
   return (
-    <View>
-      <Button onPress={()=> props.getPeople()} title='press' />
+    <View style={styles.buttonStyle}>
+      <Button onPress={()=> props.getPeople()} title='press'/>
+
+      {
+        isFetching && <Text>Loading</Text>
+      }
+      {
+        people.length ? (
+          people.map((person, i) => {
+            return <View key={i} >
+              <Text>Name: {person.name}</Text>
+              <Text>Birth Year: {person.birth_year}</Text>
+            </View>
+          })
+        ) : null
+}
+
     </View>
   )
 }
 
+styles = StyleSheet.create({
+  buttonStyle : {
+    marginTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+})
+
 const mapStateToProps = (state) => {
   return{
-    people : state.people,
+    people : state.peopleReducer,
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import peopleReducer from './peopleReducer'
 
 const rootReduce = combineReducers({
-    people: peopleReducer,
+    peopleReducer,
 })
 
-export default rootReduce
+export default rootReduce;
